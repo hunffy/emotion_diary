a021dfeffa53ea13356b5390a195351b9533aa19
ksh <sehun910124@naver.com>
2024-01-08 14:49:46 +0900

    배포 전 빌드후 오류수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -47,6 +47,11 @@ const Home = () => {
     }
   }, [diaryList, curDate]);
 
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerHTML = "감정 일기장";
+  });
+
   return (
     <div className="Home">
       <Myheader
